af067ba6860b1ad1df6f45d877ac20dfc34126ab
Karo-DRS <78567336+Karo-DRS@users.noreply.github.com>
2022-10-02 15:12:56 -0500

    Add files via upload

---
+++ _opoly_classes.py
@@ -0,0 +1,14 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.money = 1500
+        self.position = 0
+        self.jailed = 0
+        self.steal_chance = 0.5  # 50% chance default; could be changed
+
+    def print_values(self): # Test function for checking values
+        print("Current values of player", self.name, "are: ")
+        print("Money:", self.money)
+        print("Board position:", self.position)
+        print("Jailed status:", self.jailed)
+        print("Pickpocket chance: {0}%".format(self.steal_chance * 100))
