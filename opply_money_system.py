7932fbcf468fac822a1ac79a3015cd3696f26ad3
pbj17 <113928535+pbj17@users.noreply.github.com>
2022-10-19 20:04:52 -0500

    Add files via upload

---
+++ opply_money_system.py
@@ -0,0 +1,83 @@
+
+import pygame
+#still a work in progress, had to rewrite the code mutliple times. Doesnt work the right yet.
+class banker:
+
+	def _init_(self,name):
+                self.name = name
+                self.money = 1500 #tracks money
+                self.rent = rent #tracks rent
+                counter = 0 #just using as an counter
+                self.bankrupt = bankrupt
+                self.bank = bank
+                self.bank_money = bank_money
+                player_input = input()
+        #Telling that the player is bankrupt
+        def player_is_bankrupt(self.name, self.money):
+                if self.money == 0:
+                        print("Player is bankrupt")
+
+        #Rent
+        #putting in the amount of each price
+        def Rent_class(self.rent):
+                self.Electric =
+                self.WaterWorks =
+                self.Cavern =
+                self.Shell =
+                self.Park =
+                self.School =
+                self.Plaza =
+                self.Jellyfish  = 
+                self.Octo =
+                self.Castle =
+                self.Land =
+                self.Whirlpool =
+                self.Net =
+                self.Throne =
+                self.Forest =
+
+        #All players are bankrupt
+        def All_bankrupt(self.name, self.money):
+
+
+
+
+        #Landing on luxury space
+        def luxury_tax(self.money, self.name):
+                if self.name == luxury_space:
+                        self.money = self.money - 200
+                        self.bank += 200
+        #Landing on income space
+        def income_tax(self.money, self.name):
+                if self.name == income_tax:
+                        self.money = self.money - 200
+                        self.bank += 200
+      
+
+        #Players balance
+        def player_balance(self.name,self.money,self.rent):
+                print (self.name, self.money)
+ 
+        #Paying the bank out of jail
+        def escaping_jail(self.name, self.money):
+                if self.name == jail_space:
+                        print ("Want to pay $50 to get out (y/n)")
+                        if player_input ==  'y':
+                                self.money -= 50 
+                        if player_input == 'n':
+                                self.money -= 0
+                                break
+
+        #buying a space
+        #players wanting to own a space
+        def buying_a_space(self.name, self.money):
+            #stilling trying to figure out the layout of this function.
+
+   
+       #any player passing bank gives $200 to player
+       def pass_go(self.name, self.money):
+                #holding = 'GO'
+                if self.name == 'GO': #player hits go they will get $200
+                         self.money += 200
+
+
